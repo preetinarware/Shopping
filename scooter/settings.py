46d82e566feb612a6d96c5be412467eb03e887dd
Preeti narware <preetinarware97@gmail.com>
2022-01-02 23:02:37 +0530

    'fixed'

--- scooter/settings.py
+++ scooter/settings.py
@@ -161,7 +161,8 @@ STATICFILES_DIRS = (
 )
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStorage'
+#  'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 
