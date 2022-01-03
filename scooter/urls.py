c26fa9877e86147614a1a08059e79be424d39311
Preeti narware <preetinarware97@gmail.com>
2022-01-03 10:49:41 +0530

    'added'

--- scooter/urls.py
+++ scooter/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     #path('', include('send_mail.urls')),
 ]+ static(settings.STATIC_URL,
 document_root=settings.STATIC_ROOT)+\
-              static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+  static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
